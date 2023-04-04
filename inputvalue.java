32b8a479d923f83864f3a4c6257642d3a1cdca35
ANSHUGUPTA809 <128772024+ANSHUGUPTA809@users.noreply.github.com>
2023-04-04 17:05:01 +0530

    upload a file

---
+++ inputvalue.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class inputvalue {
+
+	public static void main(String[] args) {
+		int a;
+		float b;
+		String str;
+		Scanner d = new Scanner(System.in);
+		System.out.print("enter a digit=");
+		a=d.nextInt();
+	
+		System.out.print("enter a floatdigit=");
+        b=d.nextFloat();
+       
+        System.out.print("enter a string=");
+        str=d.next();
+        
+        
+	}
+
+}
